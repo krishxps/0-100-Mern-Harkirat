b476dc96341c6ae62f7e52550cfe09a97ad8cd48
Krish Patel <99328839+krishxps@users.noreply.github.com>
2024-04-22 18:59:39 -0400

    Promise working

--- Week-2/01-intro.js
+++ Week-2/01-intro.js
@@ -1,2 +1,30 @@
 // http://latentflip.com/loupe/ 
 // Website to see working of web ans sync stuff
+
+
+// Promise Example
+
+// Making Promise Function - It will take time in argument For SetTimeOut
+function promiseWorking(time){
+    // Making promise instance 
+    let p = new Promise(
+        //Resolve is function name for sync function
+        resolve=>{
+            // SetTimeOut for sync and working
+            setTimeout(function(){
+                //Function 
+                resolve();
+            },time);
+    });
+    return p;
+}
+
+function check(){
+    console.log("check");
+}
+
+const promiseWork = promiseWorking(1000);
+//Promise { <pending> }
+console.log(promiseWork);
+
+promiseWork.then(check);
