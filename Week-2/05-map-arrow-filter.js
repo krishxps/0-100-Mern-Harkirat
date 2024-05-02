a4ec030fb8ba33b31cd6dab0270f1a2c71e91317
Krish Patel <99328839+krishxps@users.noreply.github.com>
2024-05-02 17:16:27 -0400

    Create 05-map-arrow-filter.js

---
+++ Week-2/05-map-arrow-filter.js
@@ -0,0 +1,15 @@
+function sum(a,b){
+    return a+b;
+}
+
+// Arrow Function
+const sum = (a,b) => {
+    return a+b;
+}
+
+// Map Function
+// Given an arry, give me back a new arry in which every value is multipllied by 2
+const arr = [1,2,3,4,5];
+arr.map(function(value){
+    return value*2;
+})
