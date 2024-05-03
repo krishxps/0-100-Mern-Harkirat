07f3eb8234eeb52df13bf1c7707d7c471eb1de3b
Krish Patel <99328839+krishxps@users.noreply.github.com>
2024-05-02 23:05:04 -0400

    Learned map - Arrow - Filter

--- Week-2/05-map-arrow-filter.js
+++ Week-2/05-map-arrow-filter.js
@@ -3,13 +3,22 @@ function sum(a,b){
 }
 
 // Arrow Function
-const sum = (a,b) => {
+const sum2 = (a,b) => {
     return a+b;
 }
 
 // Map Function
 // Given an arry, give me back a new arry in which every value is multipllied by 2
 const arr = [1,2,3,4,5];
-arr.map(function(value){
+const mapp = arr.map(function(value){
     return value*2;
 })
+console.log("Map:",mapp);
+// Filter
+const a = arr.filter(n =>{
+    if(n % 2 == 0){
+        return true;
+    }
+    return false;
+})
+console.log("Filter:",a);
