76bdf2c7f909b142db9ec88e2488e467cbfc39c2
Krish Patel <99328839+krishxps@users.noreply.github.com>
2024-05-14 15:38:25 -0400

    zod working

--- Week-3/04-zod.js
+++ Week-3/04-zod.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
+
+const zod = require('zod');
 const port = 3000;
 
 app.use(express.json());
-app.get('/*', (req, res) => {
-    res.send('Hello World!');
-})
 
-app.listen(port, () => console.log(`server started on port https://localhost:${port}`));
+// Describing structure of input
+const input = zod.array(zod.number());
+
+app.post('/', (req, res) => {
+    const kidney = req.body.kidney;
+    
+    //will check if input is valid
+    const response = input.safeParse(kidney);
+    res.send({
+        response
+    })
+});
+
+//global catches
+app.use((err, req, res, next) => {
+    res.status(500).json({msg:'Internal server error'});
+});
+
+app.listen(port, () => console.log(`server started on port ${port}`))
