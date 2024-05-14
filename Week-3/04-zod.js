f030fe1a5aa6ec3943c79a6554fd8f7bc18b4652
Krish Patel <99328839+krishxps@users.noreply.github.com>
2024-05-14 15:57:25 -0400

    zod learned

--- Week-3/04-zod.js
+++ Week-3/04-zod.js
@@ -6,14 +6,35 @@ const port = 3000;
 
 app.use(express.json());
 
+let errorCount = 0;
+
 // Describing structure of input
 const input = zod.array(zod.number());
 
+// Object type safety description
+const schema = zod.object({
+    email : zod.string(),
+    password : zod.string(),
+    country : zod.literal("IN").or(zod.literal("CA")),
+    kidneys : zod.array(zod.number())
+});
+
 app.post('/', (req, res) => {
     const kidney = req.body.kidney;
-    
+
     //will check if input is valid
     const response = input.safeParse(kidney);
+
+    console.log(response);
+
+    res.send({
+        response
+    })
+});
+
+app.post('/test',(req,res) =>{
+    const response = schema.safeParse(req.body);
+    console.log(response);
     res.send({
         response
     })
@@ -21,6 +42,10 @@ app.post('/', (req, res) => {
 
 //global catches
 app.use((err, req, res, next) => {
+    console.log({
+        Error: err,
+        ErrorCount : ++errorCount
+    });
     res.status(500).json({msg:'Internal server error'});
 });
 
