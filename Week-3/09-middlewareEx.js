17ff0a5a7257025c5a8635f27f0bd07e7076baae
Krish Patel <99328839+krishxps@users.noreply.github.com>
2024-05-16 18:43:54 -0400

    Middleware example done

--- Week-3/09-middlewareEx.js
+++ Week-3/09-middlewareEx.js
@@ -2,19 +2,38 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-function isOldEnoughMiddleware(age){
+/////////////////////////////////// Normal Way ///////////////////////////////////
+function isOldEnoughFunction(age){
     if(age < 18){
         return true;
     }
     return false;
 }
+// it will be middleware for all functions bellow this I can do app.use(isOldEnoughMiddleware)
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    if(isOldEnoughMiddleware(req.query.age)){
+    if(isOldEnoughFunction(req.query.age)){
+        console.log('You have successfully ridden the ride with age:', req.query.age);
         res.json({ msg: 'You have successfully ridden the ride' });
     }else{
-        res.json({ msg: 'You are not old enough' });
+        console.log('You are old so req hit with age:', req.query.age);
+        res.status(403).json({ msg: 'You are old' });
     }
+});
+
+/////////////////////////////////// Next Way ///////////////////////////////////
+function isOldEnoughMiddleware(req, res, next){
+    if(isOldEnoughFunction(req.query.age)){
+        console.log('You have successfully ridden the ride with age:', req.query.age);
+        next();
+    }else{
+        console.log('You are old enough req hit with age:', req.query.age);
+        res.status(403).json({ msg: 'You are old' });
+    }
+}
+app.get('/ride', isOldEnoughMiddleware, (req, res) => {
+    res.json({ msg: 'You have successfully ridden the ride' });
 });
 
 app.listen(port, () => console.log(`server started on port http://localhost:${port}`))
