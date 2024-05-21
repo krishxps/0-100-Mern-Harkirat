7ecd998edb633853e9fbfd57997cf8faee744154
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-05-20 20:19:40 -0400

    init jsonwebtoken

---
+++ Week-4/assignment/04-mongo-with-jwt-auth/db/index.js
@@ -0,0 +1,42 @@
+const bammm = require('../../../../database.js');
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect(bammm.uri).then(() => {
+    console.log('Connected to MongoDB');
+});
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+    // Schema definition here
+    username : String,
+    password : String
+});
+
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+    username : String,
+    password : String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    // Schema definition here
+    title : String,
+    description : String,
+    imageLink : String,
+    price : Number
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
