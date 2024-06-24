ad745b62424963c0257f02f875412865c16bc0fd
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-06-24 16:47:54 -0400

    Todo App Done

--- Week-5/03-todo/README.md
+++ Week-5/03-todo/README.md
@@ -4,3 +4,11 @@ it has following features:-
 - Anyone can create a todo
 - Anyone can see their current todos
 - Anyone can make a todo as done
+
+## WHAT I MADE:
+
+- Backend
+- Express ap
+- Zod for validation
+- Mongoose for Mongo Connection
+- Mongo Db as the database
--- Week-5/03-todo/frontend/src/components/createTodo.jsx
+++ Week-5/03-todo/frontend/src/components/createTodo.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 export function CreateTodo() {
+    const [title,setTitle] =useState("");
+    const [description,setDescription] =useState("");
+
     return (
         <div style={{
             display: 'flex',
@@ -10,15 +15,36 @@ export function CreateTodo() {
             <input style={{
                 padding: 10,
                 margin: 10,
-            }} type="text" placeholder="Title" autoFocus /> <br />
+            }} type="text" placeholder="Title" autoFocus onChange={function (e) {
+                const value = e.target.value;
+                setTitle(value);
+             }} /> <br />
             <input style={{
                 padding: 10,
                 margin: 10
-            }} type="text" placeholder="Description" /> <br />
+            }} type="text" placeholder="Description" onChange={function(e){
+                const value = e.target.value;
+                setDescription(value);
+            }}/> <br />
 
             <button style={{
                 padding: 10,
                 margin: 10
+            }} onClick={()=>{
+                fetch("http://localhost:8080/todo", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        title: title,
+                        description: description
+                    }),
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                })
+                .then(async res =>{
+                    const json = await res.json();
+                    console.log(json.stringify());
+                })
             }}>Add Todo</button>
         </div>
     );
