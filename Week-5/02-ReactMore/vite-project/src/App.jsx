5edd6afd9e7517eccdaad542cef8a81810853dfc
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-06-24 14:15:30 -0400

    revision Done

--- Week-5/02-ReactMore/vite-project/src/App.jsx
+++ Week-5/02-ReactMore/vite-project/src/App.jsx
@@ -1,20 +1,24 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react"
 
 function App() {
   const [count, setCount] = useState(100);
-  function increment() {
-    let rand =  0 + (Math.random() * (100-0));
-    rand = Math.floor(rand);
-    setCount(rand);
-  }
 
   return (
-    <>
-      <h1>Learning React</h1>
-      <button onClick={increment}>Counter {count}</button>
-    </>
+    <div>
+      <h1>Click to generate random number</h1>
+      <CustomButton count={count} setCount={setCount} />
+    </div>
   )
 }
 
+function CustomButton(props) {
+  function increment() {
+    let rand = 0 + (Math.random() * (100 - 0));
+    rand = Math.floor(rand);
+    props.setCount(rand);
+  }
+  return <button onClick={increment}>Counter {props.count}</button>
+}
 
 export default App
