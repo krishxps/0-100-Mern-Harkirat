d81e1e0b33ed9dbcb0ff1a48d256a08281d81b4c
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-06-25 15:38:51 -0400

    Two Ways to Update Value

--- Week-6/01-Reactcomponentreturn/src/App.jsx
+++ Week-6/01-Reactcomponentreturn/src/App.jsx
@@ -1,18 +1,27 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
+import React ,{ useState } from 'react'
 import './App.css'
 function App() {
-  const [title,changeTitle] = useState("Hi bro")
+  const [title,changeTitle] = useState("Hi bro");
+  const [second,changeSecond] = useState("How are you?");
+
+  function changeSecTitle(){
+    const Random = Math.floor(Math.random() * 100);
+    changeSecond("My Name is " + Random);
+  }
   return (
-    <div> 
+    <React.Fragment> 
       <Button changeTitle={changeTitle}></Button>
+      <button onClick={changeSecTitle}>Update second Title</button>
       <Header title={title}></Header>
-      <Header title="Title 2nd"></Header>
-    </div>
+      <Header title={second}></Header>
+    </React.Fragment>
   )
 }
 function Button({changeTitle}){
-  return <button onClick={() => changeTitle(Math.floor(Math.random() * 100))}>Click me to change Title</button>;
+  return <button onClick={() => changeTitle(Math.floor(Math.random() * 100))}>
+    Click me to change Title
+  </button>;
 }
 function Header({title}){
   return <div>{title}</div>
