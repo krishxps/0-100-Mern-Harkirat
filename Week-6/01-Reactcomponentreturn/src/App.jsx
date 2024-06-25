0b598a01145370f38d4a82ec8cd2e22eee394373
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-06-25 16:25:57 -0400

    Todo With react randering

--- Week-6/01-Reactcomponentreturn/src/App.jsx
+++ Week-6/01-Reactcomponentreturn/src/App.jsx
@@ -1,6 +1,11 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
-import React ,{ useState } from 'react'
+// import React, { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
+////////////////////////////////////////////////////////////////////////////////
+//  Re-Render
+////////////////////////////////////////////////////////////////////////////////
 // function App() {
 //   const [title,changeTitle] = useState("Hi bro");
 //   const [second,changeSecond] = useState("How are you?");
@@ -26,28 +31,79 @@ import './App.css'
 // function Header({title}){
 //   return <div>{title}</div>
 // }
-function App(){
+
+////////////////////////////////////////////////////////////////////////////////
+//  Re-Render - Better Way
+////////////////////////////////////////////////////////////////////////////////
+// function App(){
+//   return <>
+//     <HeaderWithButton/>
+//     <Header title="IDK" />
+//     <Header title="I am Sure" />
+//   </>
+// }
+
+// // This Will Prevent For re-rendering whole page insted it will render only this object
+// function HeaderWithButton(){
+//   const [title,changeTitle] = useState("This will Change when you click on button");
+//   return <>
+//     <button onClick={()=>changeTitle(Math.floor(Math.random() * 100))}>
+//       Click To change
+//     </button>
+//     <Header title={title}></Header>
+//   </>
+// }
+
+// // memo lets you skip re-rendering a component when its props are unchanged.
+// const Header = React.memo(function Header({title}){
+//   return <div>{title}</div>
+// });
+
+////////////////////////////////////////////////////////////////////////////////
+//  Keys in React
+////////////////////////////////////////////////////////////////////////////////
+
+let counter = 3;
+function App() {
+  const [todo, setTodo] = useState([{
+    id: 1,
+    title: "Go to gym",
+    description: "Go to gym Today"
+  },
+  {
+    id: 2,
+    title: "Lol",
+    description: "Go to gym Today Lol"
+  }, {
+    id: 3,
+    title: "Hahahah",
+    description: "Go to gym Today Hahahah"
+  }]);
+
+  function addTodo() {
+    setTodo([...todo,{
+      id: ++counter,
+      title: Math.random(),
+      description: Math.random()
+    }])
+  }
+
   return <>
-    <HeaderWithButton/>
-    <Header title="IDK" />
-    <Header title="I am Sure" />
+    <button onClick={addTodo}>Add Todo</button>
+    {/* We need key to uniquely identify each component so in future react can figure what to change so when state change it can help in re-rendering*/}
+    {todo.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description}></Todo>)}
   </>
 }
 
-// This Will Prevent For re-rendering whole page insted it will render only this object
-function HeaderWithButton(){
-  const [title,changeTitle] = useState("This will Change when you click on button");
-  return <>
-    <button onClick={()=>changeTitle(Math.floor(Math.random() * 100))}>
-      Click To change
-    </button>
-    <Header title={title}></Header>
-  </>
+function Todo({ title, description }) {
+  return <div>
+    <h1>
+      {title}
+    </h1>
+    <h4>
+      {description}
+    </h4>
+  </div>
 }
-
-// memo lets you skip re-rendering a component when its props are unchanged.
-const Header = React.memo(function Header({title}){
-  return <div>{title}</div>
-});
 
 export default App
