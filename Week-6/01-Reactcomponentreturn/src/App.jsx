3b4af959668973478a4d99024e57babba0355a6b
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-06-25 15:32:42 -0400

    Change Title Button Exercise

--- Week-6/01-Reactcomponentreturn/src/App.jsx
+++ Week-6/01-Reactcomponentreturn/src/App.jsx
@@ -1,13 +1,19 @@
+/* eslint-disable react/prop-types */
+import { useState } from 'react'
 import './App.css'
 function App() {
+  const [title,changeTitle] = useState("Hi bro")
   return (
     <div> 
-      <Header title="Title"></Header>
+      <Button changeTitle={changeTitle}></Button>
+      <Header title={title}></Header>
       <Header title="Title 2nd"></Header>
     </div>
   )
 }
-
+function Button({changeTitle}){
+  return <button onClick={() => changeTitle(Math.floor(Math.random() * 100))}>Click me to change Title</button>;
+}
 function Header({title}){
   return <div>{title}</div>
 }
