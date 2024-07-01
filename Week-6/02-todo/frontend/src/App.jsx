4bc386907f11dc7b221c2d6582a620a85d8d18da
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-07-01 17:41:46 -0400

    Get Specific task by ID

--- Week-6/02-todo/frontend/src/App.jsx
+++ Week-6/02-todo/frontend/src/App.jsx
@@ -1,25 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import {CreateTodo} from './components/createTodo'
-import { Todos } from './components/Todos'
-import { useEffect } from 'react';
-function App() {
-  const [todos,setTodo] = useState([]);  
-  useEffect(()=>{
-    setInterval(() => {
-      fetch("https://sum-server.100xdevs.com/todos")
-      .then(async (res) => {
-        const data = await res.json()
-        setTodo(data.todos);
-      });
-    }, 5 * 1000);
-  },[]);
+import { useState } from "react";
+import { useEffect } from "react";
 
-  return (
-    <div >
-      <CreateTodo />
-      <Todos todos={todos}></Todos>
-    </div>
-  )
+function App() {
+  return <div>
+    <Todo id={1} />
+  </div>
 }
-export default App
+
+// eslint-disable-next-line react/prop-types
+function Todo({id}) {
+  const [todo, setTodo] = useState({});
+
+  useEffect(() => {
+    fetch("https://sum-server.100xdevs.com/todo?id=" + id)
+      .then(async function(res) {
+        const json = await res.json();
+        setTodo (json.todo);
+      })
+  }, [id])
+
+  return <div>
+    <h1>
+      {todo.title}
+    </h1>
+    <h4>
+      {todo.description}
+    </h4>
+  </div>
+}
+
+export default App;
