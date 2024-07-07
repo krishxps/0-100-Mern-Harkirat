ec0441f826026025d95114931dbeb8b0c6787d23
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-07-07 18:47:06 -0400

    Remove todo app

--- My-Projects/My-Todo/frontend/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-const Home = () => (
-  <div>
-    <h1>Welcome to the Todo App</h1>
-  </div>
-);
-
-export default Home;
--- My-Projects/My-Todo/frontend/src/pages/Todos.jsx
+++
@@ -1,9 +0,0 @@
-import TodoList from '../components/TodoList';
-
-const Todos = () => (
-  <div>
-    <TodoList />
-  </div>
-);
-
-export default Todos;
