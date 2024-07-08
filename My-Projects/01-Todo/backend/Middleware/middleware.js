f97a17d09a3f630f8458c0cc7b4d0325a3908a0b
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-07-08 14:38:13 -0400

    Fixed Admin Middleware

--- My-Projects/01-Todo/backend/Middleware/middleware.js
+++ My-Projects/01-Todo/backend/Middleware/middleware.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const {jwtToken} = require('../../../../database')
+const {users} = require('../Database/userSchema');
 
 const authenticateToken = (req, res, next) => {
     const token = req.header('token')?.split(' ')[1]; 
@@ -17,15 +18,11 @@ const authenticateToken = (req, res, next) => {
     }
 };
 
-const adminMiddleware = (req , res, next) => {
-    if(!req.user){
-        return res.status(401).json({ Message: "Access Denied: No User Authenticated" });
-    }
-
-    if(!req.user.isAdmin){
+const adminMiddleware = async(req , res, next) => {
+    const User = await users.findOne({ _id: req.user._id });
+    if(!User.isAdmin){
         return res.status(403).json({ Message: "Access Denied: Admins Only" });
     }
-
     next();
 } 
 
