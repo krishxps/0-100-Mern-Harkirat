a624ec1b11e59713739cb09a3416df615c3e60ab
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-07-13 15:44:57 -0400

    Pomodoro init

---
+++ My-Projects/01-Todo/frontend/src/components/study/main.jsx
@@ -0,0 +1,8 @@
+import Header from "./header"
+import Timer from "./timer"
+export default function Main() {
+    return <div>
+        <Header />
+        <Timer />
+    </div>
+}
---
+++ My-Projects/01-Todo/frontend/src/components/study/timer.jsx
@@ -0,0 +1,6 @@
+
+export default function Timer() {
+    return <div>
+        <h1>Timer</h1>
+    </div>
+}
