bc519ac6564aeda9fc6c099f40a9a44bdb7be5f8
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-07-15 16:17:01 -0400

    Paytm basic init

--- Week-8/02-dukan-dashboard-tailwind/src/App.jsx
+++ Week-8/02-dukan-dashboard-tailwind/src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css';
-import { RevenueCard } from './components/RevenueCard';
+import "./App.css";
+import { RevenueCard } from "./components/RevenueCard";
 function App() {
   // https://dashboard-clone-dukaan.vercel.app/
-  return <>
-  <div className="grid grid-cols-3">
-    <RevenueCard title="Amount Pending" orderCount={10} amount={1000} />
-  </div>
-  </>
+  // https://heroicons.com/
+  return (
+    <>
+      <div className="grid grid-cols-3">
+        <RevenueCard title="Amount Pending" orderCount={10} amount={1000} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
