a24fbb42d70f53908b276699a4533ca2dc4b46ec
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-07-15 19:08:58 -0400

    Fix Account Transfer

--- My-Projects/paytm-basic/backend/routes/account.js
+++ My-Projects/paytm-basic/backend/routes/account.js
@@ -25,13 +25,14 @@ router.get("/balance", authMiddleware, async (req, res) => {
 });
 
 router.post("/transfer", authMiddleware, async (req, res) => {
+    // Session is used to handle transactions in the database because it is an asynchronous operation that returns a promise that resolves when the transaction is committed. if any reason fails, the transaction is aborted and the promise is rejected. 
     const session = await mongoose.startSession();
 
     session.startTransaction();
     const { amount, to } = req.body;
 
     const account = await Account.findOne({ userId: req.userId }).session(session);
-
+    
     if (!account || account.balance < amount) {
         await session.abortTransaction();
         return res.status(400).json({
@@ -39,8 +40,8 @@ router.post("/transfer", authMiddleware, async (req, res) => {
         });
     }
 
-    const toAccount = await Account.findOne({ userId: to }).session(session);
-
+    const toAccount = await Account.findOne({ userID: to }).session(session);
+    
     if (!toAccount) {
         await session.abortTransaction();
         return res.status(400).json({
@@ -49,9 +50,10 @@ router.post("/transfer", authMiddleware, async (req, res) => {
     }
 
     await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
-    await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
+    await Account.updateOne({ userID: to }, { $inc: { balance: amount } }).session(session);
 
     await session.commitTransaction();
+
     res.json({
         message: "Transfer successful"
     });
