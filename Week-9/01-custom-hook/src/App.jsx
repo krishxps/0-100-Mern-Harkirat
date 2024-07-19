faa1f76b75198813a953a20ffa359d9f671b352b
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-07-19 13:30:20 -0400

    Fetch Data every n seconds

--- Week-9/01-custom-hook/src/App.jsx
+++ Week-9/01-custom-hook/src/App.jsx
@@ -71,17 +71,25 @@ import axios from 'axios'
 //     </>
 //   )
 // }
-function useTodos() {
+
+function useTodos(n) {
   const [loading, setLoading] = useState(true);
   const [todos, setTodos] = useState([])
 
-  useEffect(() => {
+  function getData() {
     axios.get("https://sum-server.100xdevs.com/todos")
       .then(res => {
         setTodos(res.data.todos);
         setLoading(false);
       })
-  }, [])
+  }
+
+  useEffect(() => {
+    setInterval(() => {
+      getData();
+    }, n * 1000)
+    getData();
+  }, [n])
 
   return {
     todos: todos,
@@ -90,19 +98,21 @@ function useTodos() {
 }
 
 function App() {
-  const { todos, loading } = useTodos();
+  const { todos, loading } = useTodos(5);
 
   if (loading) {
     return <div>
       Loading...
     </div>
   }
+
   return (
     <>
       {todos.map(todo => <Track todo={todo} />)}
     </>
   )
 }
+
 function Track({ todo }) {
   return <div>
     {todo.title}
