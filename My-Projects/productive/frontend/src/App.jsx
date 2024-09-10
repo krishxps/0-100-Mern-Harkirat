8fa8fe83ea3e1a224db75878c98ecef6207d2bd0
Krish Mac <99328839+krishxps@users.noreply.github.com>
2024-09-10 07:46:45 -0400

    Testing Old Things

--- My-Projects/productive/frontend/src/App.jsx
+++ My-Projects/productive/frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import { Main } from "./Routes/main";
 import { SettingsPage } from "./Routes/SettingPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/settings" element={<SettingsPage />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/settings" element={<SettingsPage />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
+  );
 }
